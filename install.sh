a8eace54e574375ba9a821357f9a3e53cecd1a28
caoyuanzhi <caoyuanzhi@kylinos.cn>
2022-03-17 17:20:06 +0800

    Fix install.sh bash error.

--- install.sh
+++ install.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 
-if [ $DEVMODE == "true" ]
+if [ $DEVMODE = "true" ]
 then
-   $IMAGEPATH='image-list/images_dev'
+   export IMAGEPATH='image-list/images_dev'
    echo '采用开发者部署模式:
 请确认内部开发者源地址可以访问
 harbor.kylincloud.org   registry.kylincloud.org.'
-elif
-   $IMAGEPATH='image-list/images'
+else
+   export IMAGEPATH='image-list/images'
    echo '采用生产环境部署模式:
 请确认生产镜像已展开,registry服务已就绪
 ,且各个服务端的对应registry.kylincloud.org域名的hosts映射已配置.'
 fi
 
-export IMAGELIST=${IMAGEPATH}-${ARCH}.yaml
-
-
 ARGS=''
 
 ARCH=$(uname -m)
 
-if [ $ARCH = "arm64" ]
+if [ $ARCH = "aarch64" ]
 then
    export ARCH=arm64
 elif [ $ARCH = "x86_64" ]
 then
    export ARCH=amd64
 fi
+
+export IMAGELIST=${IMAGEPATH}-${ARCH}.yaml
 
 source scripts/ha-common.sh
 source scripts/app-common.sh
