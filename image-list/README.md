03dc049957629af282e90699b00c7512dd927a5f
wangqiwei <wangqiwei@kylinos.cn>
2022-03-25 14:10:14 +0800

    更新镜像列表，更新镜像列表组织方式

---
+++ image-list/README.md
@@ -0,0 +1,5 @@
+# 更新镜像列表
+
+更新镜像列表请更新 `images_dev-${arch}.yaml` 文件
+
+更新对应的文件之后，执行 `./mklist.sh` 更新`images-${arch}.yaml` 文件
--- image-list/mklist.sh
+++ image-list/mklist.sh
@@ -1,31 +1,16 @@
-> images-amd64.yaml
-> images-arm64.yaml
-> images_dev-amd64.yaml
-> images_dev-arm64.yaml
+#!/bin/bash
 
-for ARCH in amd64 arm64;do
+for arch in arm64 amd64; do
+echo "global:" > images-${arch}.yaml
+echo "  images:" >>  images-${arch}.yaml
+cat images_dev-${arch}.yaml | tail -n +3 | while read line; do 
+    newline=""
+    echo $line | grep harbor >/dev/null && {
+        key=$(echo $line | awk '{print $1}')
+	    value=$(echo $line | awk '{print $2}'| sed 's=\"==g' | sed 's=-'${arch}'==g' | awk -F'/' '{print "registry.kylincloud.org:4001/solution/"$2"/'${arch}'/"$NF}' | sed 's=solution-==g')
+	newline="$key \"$value\""
+    } || newline=$line
+    echo $newline | awk '{printf "    %-30s%s\n",$1,$2}' >> images-${arch}.yaml
 
-echo ========= images-${ARCH}.yml ========
-echo images: >images-${ARCH}.yaml
-cat ${ARCH} | while read line; do echo $line |grep harbor >/dev/null && {
-   echo $line | sed 's=-'${ARCH}'==g' | awk -F'/' '{print "registry.kylincloud.org:4001/solution/"$2"/'${ARCH}'/"$NF}' | sed 's=solution-==g'
-} || echo "$line" | sed 's=-'${ARCH}'==g'; done | sort | while read product; do
-    key=$(echo ${product} | awk -F'/|:' '{print $(NF-1)}' | sed 's=-'${ARCH}'==g'| sed 's=-12==g' | sed 's=-[a-z]=\U&=g' | sed 's=-==g')
-    value=$product;
-
-    echo -e "$key:\t\t$value" | awk '{printf "  %-30s\"%-15s\"\n",$1,$2}' >>images-${ARCH}.yaml
-    echo -e "$key:\t\t$value" | awk '{printf "  %-30s\"%-15s\"\n",$1,$2}'
-done 
-
-echo ======= images_dev-${ARCH}.yml =======
-echo images: >images_dev-${ARCH}.yaml
-cat ${ARCH} | while read line; do
-
-key=$(echo $line | awk -F'/|:' '{print $(NF-1)}' | sed 's=-'${ARCH}'==g' | sed 's=-12==g' | sed 's=-[a-z]=\U&=g' | sed 's=-==g')
-value=$line;
-
-echo -e "$key:\t\t$value" | awk '{printf "  %-30s\"%-15s\"\n",$1,$2}';
-
-done >images_dev-${ARCH}.yaml
-
+done
 done
