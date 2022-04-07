a47cf8b73c0b0907d0de8fe6a12c146122b9a1de
wangqiwei <wangqiwei@kylinos.cn>
2022-04-07 16:41:53 +0800

    新增maxscale镜像，更新mysql初始化镜像

--- image-list/mklist.sh
+++ image-list/mklist.sh
@@ -7,9 +7,9 @@ cat images_dev-${arch}.yaml | tail -n +3 | while read line; do
     newline=""
     echo $line | grep harbor >/dev/null && {
         key=$(echo $line | awk '{print $1}')
-	    value=$(echo $line | awk '{print $2}'| sed 's=\"==g' | sed 's=-'${arch}'==g' | awk -F'/' '{print "registry.kylincloud.org:4001/solution/"$2"/'${arch}'/"$NF}' | sed 's=solution-==g')
+	    value=$(echo $line | awk '{print $2}'| sed 's=\"==g' | sed 's=-'${arch}'==g' | awk -F'/' '{print "registry.kylincloud.org:4001/solution/"$2"/'${arch}'/"$NF}' | sed 's=solution-==g' | sed 's=-'${arch}'==g')
 	newline="$key \"$value\""
-    } || newline=$line
+} || newline=$(echo $line | sed 's=-'${arch}'==g')
     echo $newline | awk '{printf "    %-30s%s\n",$1,$2}' >> images-${arch}.yaml
 
 done
