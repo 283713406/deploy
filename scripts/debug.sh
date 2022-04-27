82e28436b23ef91ef9c7c9fa446368747c702d29
caoyuanzhi <caoyuanzhi@kylinos.cn>
2022-04-27 11:46:23 +0800

    增加商店template函数修复格式错误

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -6,3 +6,10 @@ template-app-tianyu() {
         -f values/apps-values.yaml  -f ${IMAGELIST} \
         -f values/global-values.yaml > /tmp/tianyu.yaml
 }
+
+
+template-app-softshop() {
+    helm template ${ARGS}  application/softshop/  \
+        -f values/apps-values.yaml  -f ${IMAGELIST} \
+        -f values/global-values.yaml > /tmp/softshop.yaml
+}
